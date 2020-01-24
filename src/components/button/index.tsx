c12c1150e09f4ee71c0c859cbcb17120a2b2c596
Paul Pan <panjunhong05@gmail.com>
2020-01-24 01:24:00 -0800

    Revised membership URL

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,7 +4,7 @@ import './style.less';
 const Button: React.FC = () => {
   return (
     <div className="buttonContainer">
-      <a href="https://acmucsd.com/login">
+      <a href="https://members.acmucsd.com/login">
         <div className="button">
         Login
         </div>

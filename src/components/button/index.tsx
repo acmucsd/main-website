e9b91e5f5e1819d2666a10ec1c83f56a1ff804c4
leestanley <stanleylee91776@gmail.com>
2020-01-24 01:06:41 -0800

    link change

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,7 +3,7 @@ import './style.less';
 
 const Button: React.FC = () => {
   return (
-    <a className="link" href="https://acmucsd.com/">
+    <a className="link" href="https://members.acmucsd.com/">
       <div className="button">Student Portal</div>
     </a>
   );

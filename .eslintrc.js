7bf4fd2f41b0fd179d87833eb345f98317198fed
Paul Pan <panjunhong05@gmail.com>
2020-04-24 10:54:15 -0700

    Removed one unnecessary eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,14 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
     'airbnb-typescript',
+    'prettier',
   ],
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
   },
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    'react/jsx-one-expression-per-line': 'off',
     'react/no-array-index-key': 'off',
     'operator-linebreak': 'off',
     'react/prop-types': 'off',
-  }
+  },
 };
--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -8,12 +8,18 @@ const AboutUs: React.FC = () => (
     <h1>Join our member-driven computing community</h1>
     <div className="about-info">
       <div className="about-description">
-        <p>
+        <span>
           ACM was founded because we saw a need for a more together student community among
-          individuals interested in technology - computing, &nbsp;<b>design</b>, <b>hacking</b>,{' '}
-          <b>cybersecruity</b>, and <b>graphics</b> to name a few. We welcome you to drop by our
-          events!
-        </p>
+          individuals interested in technology - computing, &nbsp;
+        </span>
+        <b>design</b>
+        <span>, </span>
+        <b>hacking</b>
+        <span>, </span>
+        <b>cybersecruity</b>
+        <span>, and </span>
+        <b>graphics</b>
+        <span> to name a few. We welcome you to drop by our events!</span>
         <p>
           Interested in getting more involved? Join the Diamond Staff program to volunteer for fun
           socials or mentor technical workshops for 2000+ students per quarter.

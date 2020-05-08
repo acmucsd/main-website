63f136d266832decbec690f4f42f9f8859ed44c7
Steven Steiner <steets250@gmail.com>
2020-05-08 14:55:09 -0700

    New Date/Time Generation

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,3 +1,5 @@
+import { EventObject } from './api/EventsAPI';
+
 /**
  * Determines if given string is a valid website link.
  * @param {string} str The string containing a potential URL.
@@ -36,4 +38,72 @@ export const getAbsoluteURL = (str: string): string | undefined => {
   }
 
   return str;
+};
+
+/**
+ * Contains timing information for an event.
+ * @param {string} date The date description.
+ * * @param {string} time The time description.
+ */
+export type EventTime = {
+  date: string;
+  time: string;
+};
+
+/**
+ * Converts an event's date information into human readable descriptions.
+ * @param {EventObject} event The event to be processed.
+ * @return {EventTime} English strings describing date and time.
+ */
+export const getDateTime = (event: EventObject): EventTime => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const startDate = new Date(event.start);
+  const endDate = new Date(event.end);
+  const result: EventTime = { date: '', time: '' };
+
+  if (startDate.getDate() !== endDate.getDate()) {
+    result.date = `${months[startDate.getMonth()]} ${startDate.getDate()} - 
+    ${months[endDate.getMonth()]} ${endDate.getDate()}`;
+  } else {
+    result.date = `${months[startDate.getMonth()]} ${startDate.getDate()}`;
+  }
+
+  const processPeriod = (h: number): string => (h < 12 || h === 24 ? 'am' : 'pm');
+
+  const processTime = (h: number, m: number): string => {
+    let time = '';
+
+    if (h === 0 || h === 24) {
+      time = '12';
+    } else if (h < 13) {
+      time = h.toString();
+    } else if (h < 24) {
+      time = (h % 12).toString();
+    }
+
+    time += m > 9 ? `:${m}` : `:0${m}`;
+
+    time += processPeriod(h);
+
+    return time;
+  };
+
+  result.time = `${processTime(startDate.getHours(), startDate.getMinutes())} - 
+  ${processTime(endDate.getHours(), endDate.getMinutes())}`;
+
+  return result;
 };

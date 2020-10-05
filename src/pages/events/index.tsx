4da18a595d9ddbe3a0d508373554649e162b1b4a
Andrea Sudharta <asudhart@ucsd.edu>
2020-10-05 11:48:10 -0700

    Scrolling events (#72)
    * added horizontal scrolling
    
    * added scrollbar at bottom of events
    
    * added draggable scrolling
    
    * linting fixes
    
    * moved em calculation out of effect hook
    
    * restored events array as second arg to useeffect
    
    * netlify pls
    
    * bugs related to scrolling direction and focus highlight fixed
    
    * linting fixes :/
    
    * display all events

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { EventsArray, EventObject, getAllEvents } from '../../api/EventsAPI';
 import { isURL, getAbsoluteURL, getDateTime } from '../../utils';
 
 import './style.less';
 
 const Events: React.FC = () => {
-  const [events, setEvents] = React.useState<EventsArray>();
+  const [events, setEvents] = useState<EventsArray>();
+  const [dragging, toggleDragging] = useState(false);
 
   const updateEvents = async (): Promise<void> => {
     const eventsArray: EventsArray | undefined = await getAllEvents();
     if (eventsArray === undefined) {
       setEvents(new Array<EventObject>());
     }
-    setEvents(eventsArray?.slice(0, 6));
+    setEvents(eventsArray);
   };
 
   React.useEffect(() => {
@@ -21,6 +21,39 @@ const Events: React.FC = () => {
       updateEvents();
     }
   }, [events]);
+
+  const handleMove = (e: any) => {
+    if (!dragging) {
+      return;
+    }
+    const em = parseFloat(
+      window.getComputedStyle(document.getElementsByClassName('events-container')[0]).fontSize,
+    );
+    const isTouch = e.movementX === '';
+    let deltaX = 0;
+    if (isTouch) {
+      deltaX = e.touches[0].clientX;
+    } else {
+      e.preventDefault();
+      deltaX = -1 * e.movementX;
+    }
+    const firstEvent = document.getElementsByClassName('event')[0] as HTMLElement;
+    const cardWidth = em * 20;
+    const minMargin = events ? em - events.length * 20 * em : cardWidth;
+    const oldmargin = parseFloat(window.getComputedStyle(firstEvent).marginLeft);
+    const newmargin = oldmargin - deltaX;
+    if (newmargin >= minMargin && newmargin <= em) {
+      firstEvent.style.marginLeft = `${oldmargin - deltaX}px`;
+    }
+  };
+
+  const handleMoveStart = () => {
+    toggleDragging(true);
+  };
+
+  const handleMoveEnd = () => {
+    toggleDragging(false);
+  };
 
   return (
     <div className="events-page">
@@ -31,25 +64,41 @@ const Events: React.FC = () => {
           of student members.
         </p>
       </div>
-      <div className="events">
-        {events &&
-          events.map((value, index) => {
-            const timing = getDateTime(value);
-            return (
-              <div className="event" key={index}>
-                <h2>{timing.date}</h2>
-                <h1 className={`index${index % 5}`}>{value.title}</h1>
-                {isURL(value.location) ? (
-                  <a className="link" href={getAbsoluteURL(value.location)}>
+      <div className="events-container">
+        <div
+          className="events"
+          onMouseDown={handleMoveStart}
+          onMouseMove={handleMove}
+          onMouseUp={handleMoveEnd}
+          onMouseLeave={handleMoveEnd}
+          onTouchStart={handleMoveStart}
+          onTouchMove={handleMove}
+          onTouchCancel={handleMoveEnd}
+          onTouchEnd={handleMoveEnd}
+          onDragStart={() => false}
+          role="menuitem"
+          tabIndex={0}
+        >
+          {events &&
+            events.map((value, index) => {
+              const timing = getDateTime(value);
+              return (
+                <div className="event" key={index}>
+                  <h2>{timing.date}</h2>
+                  <h1 className={`index${index % 5}`}>{value.title}</h1>
+                  {isURL(value.location) ? (
+                    <a className="link" href={getAbsoluteURL(value.location)}>
+                      <h3>{value.location}</h3>
+                    </a>
+                  ) : (
                     <h3>{value.location}</h3>
-                  </a>
-                ) : (
-                  <h3>{value.location}</h3>
-                )}
-                <h3>{timing.time}</h3>
-              </div>
-            );
-          })}
+                  )}
+                  <h3>{timing.time}</h3>
+                </div>
+              );
+            })}
+          <div aria-hidden="true" className="event end" />
+        </div>
       </div>
     </div>
   );

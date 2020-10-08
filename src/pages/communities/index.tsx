991556ea23493535d54ac166a7c32bf1fd762794
Emily Nguyen <emilyhuongnguyen@gmail.com>
2020-10-08 14:47:40 -0700

    Update CSS across site (#94)
    * Color Variable Refactor (#84)
    
    * Add Communities section (#76)
    
    * Update style for current website (#73)
    
    * Add communities Section
    
    * Revert "Add Communities section (#76)" (#77)
    
    This reverts commit c552b10ec8e32f4b45f0be0b04616904908b99cd.
    
    * Update website for kickoff (#79)
    
    * Update style for current website (#73)
    
    * Add about section (#74)
    
    * Add Get Involved (#75)
    
    * Add communities Section (#78)
    
    * Add Missing Packages
    
    * Update links
    
    Co-authored-by: joey <45801807+dangjoey@users.noreply.github.com>
    Co-authored-by: Steven Steiner <steets250@gmail.com>
    Co-authored-by: Emily Nguyen <eminguyen@Emilys-MBP.lan>
    
    * Emergency Fixes (#80)
    
    * entrepreneurship
    
    * Update index.tsx
    
    * THINGS
    
    * Squeeze Fix (#81)
    
    * Squeeze Fix
    
    * Linting
    
    * This Better Work (#82)
    
    * Update index.tsx (#83)
    
    * Add color variables
    
    Co-authored-by: Emily Nguyen <emilyhuongnguyen@gmail.com>
    Co-authored-by: Steven Steiner <steets250@gmail.com>
    Co-authored-by: Emily Nguyen <eminguyen@Emilys-MBP.lan>
    
    * Update headers (#88)
    
    * Update headers
    
    * Update header descriptions
    
    * Fix header spacing (#89)
    
    * Fix Communities Spacing (#90)
    
    * Fix communities spacing
    
    * Undo event api change
    
    * Update images (#91)
    
    * Get involved (#92)
    
    * Update get involved page css
    
    * Update hover effect
    
    * Update home screen CSS and add arrow (#93)
    
    * Fix lint issues
    
    * Fix lint again
    
    * Fix lint once again
    
    * Fix scroll listener
    
    * Fix lint once more
    
    * Fix return
    
    * Update style.less
    
    * Remove padding
    
    * Fix get involved spacing
    
    Co-authored-by: joey <45801807+dangjoey@users.noreply.github.com>
    Co-authored-by: Steven Steiner <steets250@gmail.com>
    Co-authored-by: Emily Nguyen <eminguyen@Emilys-MBP.lan>
    Co-authored-by: Stanley Lee <stanleylee91776@gmail.com>

--- src/pages/communities/index.tsx
+++ src/pages/communities/index.tsx
@@ -7,42 +7,50 @@ import HackLogo from '../../assets/hack_logo.svg';
 import InnovateLogo from '../../assets/innovate_logo.svg';
 
 const Communities: React.FC = () => (
-  <div className="communities-page">
-    <div className="communities-header">
+  <div className="communities">
+    <div className="header">
       <h1>Communities</h1>
-      <p>ACM consists of 5 orgs that each focus on a specific area of tech.</p>
+      <p>Looking for a more specialized focus? Check out our communities!</p>
     </div>
 
     <div className="communities-container">
       <a className="community" href="https://ai.acmucsd.com/">
         <img src={AiLogo} alt="Ai Logo" />
         <div className="info">
-          <h1 className="logo-name ai"> ACM AI</h1>
-          <h2>Machine Learning</h2>
+          <div className="info-container">
+            <h1 className="logo-name ai"> ACM AI</h1>
+            <h2>Artificial Intelligence</h2>
+          </div>
         </div>
       </a>
 
       <a className="community" href="https://acmurl.com/cyberdiscord">
         <img src={CyberLogo} alt="Cyber Logo" />
         <div className="info">
-          <h1 className="logo-name cyber">ACM Cyber</h1>
-          <h2>Cybersecurity</h2>
+          <div className="info-container">
+            <h1 className="logo-name cyber">ACM Cyber</h1>
+            <h2>Computer Security</h2>
+          </div>
         </div>
       </a>
 
       <a className="community" href="https://acmurl.com/design">
         <img src={DesignLogo} alt="Design Logo" />
         <div className="info">
-          <h1 className="logo-name design">ACM Design</h1>
-          <h2>Coming Soon</h2>
+          <div className="info-container">
+            <h1 className="logo-name design">ACM Design</h1>
+            <h2>Human Computer Interaction</h2>
+          </div>
         </div>
       </a>
 
       <a className="community" href="https://acmurl.com/joinhack">
         <img src={HackLogo} alt="Hack Logo" />
         <div className="info">
-          <h1 className="logo-name hack">ACM Hack</h1>
-          <h2>Software Engineering</h2>
+          <div className="info-container">
+            <h1 className="logo-name hack">ACM Hack</h1>
+            <h2>Software Engineering</h2>
+          </div>
         </div>
       </a>
 
@@ -52,8 +60,10 @@ const Communities: React.FC = () => (
       >
         <img src={InnovateLogo} alt="Innovate Logo" />
         <div className="info">
-          <h1 className="logo-name innovate">ACM Innovate</h1>
-          <h2>Entrepreneurship</h2>
+          <div className="info-container">
+            <h1 className="logo-name innovate">ACM Innovate</h1>
+            <h2>Entrepreneurship</h2>
+          </div>
         </div>
       </a>
     </div>
--- src/utils.tsx
+++ src/utils.tsx
@@ -10,6 +10,7 @@ export const isURL = (str: string | undefined): boolean => {
     return false;
   }
 
+  /* eslint-disable */
   const pattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
@@ -19,6 +20,7 @@ export const isURL = (str: string | undefined): boolean => {
       '(\\#[-a-z\\d_]*)?$', // fragment locator
     'i',
   );
+  /* eslint-disable */
 
   return !!pattern.test(str);
 };

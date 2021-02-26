e8f4dad8dd6d697e8efa46be9608881f96031729
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-02-26 15:01:10 -0800

    made nav-bar mobile responsive

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react"
 import { Link, navigate, graphql, useStaticQuery } from "gatsby"
 import "./style.less"
 import ACMLogo from "../../assets/acm.svg"
+import MenuButtonIcon from "../../assets/menubutton.svg"
+import CloseMenuButton from "../../assets/closemenubutton.svg"
 
 const navLinks = [
   { to: "/contact", text: "Contact" },
@@ -11,6 +13,10 @@ const navLinks = [
 ]
 
 const NavigationBar: React.FC = () => {
+  const [menuState, setMenuState] = useState(false)
+  const toggleMenu = () => {
+    setMenuState(!menuState)
+  }
   return (
     <div className="fixedNav">
       <div className="navigation-bar">
@@ -30,6 +36,47 @@ const NavigationBar: React.FC = () => {
           ))}
         </ul>
       </div>
+
+      {!menuState && (
+        <div className="navbar-mobile-div">
+          <div className="navbar-mobile">
+            <Link to="/">
+              <img src={ACMLogo} alt="ACM Logo" />
+            </Link>
+            <button className="icon" onClick={toggleMenu} type="button">
+              <img className="menuicon" src={MenuButtonIcon} alt="Menu Icon" />
+            </button>
+          </div>
+        </div>
+      )}
+      {menuState && (
+        <div className="mobile-links">
+          <button className="icon" onClick={toggleMenu} type="button">
+            <img
+              className="menuicon"
+              src={CloseMenuButton}
+              color="white"
+              alt="Menu Icon"
+            />
+          </button>
+          {navLinks.map(link => (
+            <Link to={link.to}>{link.text}</Link>
+          ))}
+          {/* <Link
+            to="#contact"
+            onClick={(): void => {
+              toggleMenu()
+              scrollToContacts()
+            }}
+          >
+            Contact
+          </Link> */}
+          <a href="https://members.acmucsd.com/login" onClick={toggleMenu}>
+            Login
+          </a>
+        </div>
+      )}
+
       <div className="rainbow" />
     </div>
   )

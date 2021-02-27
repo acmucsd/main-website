493b0bab8d106717838e7b9842258d3981a0e187
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-02-26 16:23:28 -0800

    created responsive footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import NavigationBar from "./NavigationBar/index.tsx"
+import Footer from "./Footer/index.tsx"
 
 import "./layout.less"
 
@@ -32,20 +33,13 @@ const Layout = ({ children }) => {
         style={{
           margin: `0 auto`,
           maxWidth: 960,
+          minHeight: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
       </div>
+      <Footer />
     </>
   )
 }

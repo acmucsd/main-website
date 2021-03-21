a0dd881ca7f29efcecbb9093086ef3ad0f6ec1aa
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-03-21 13:58:46 -0700

    updated mobile footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,11 +13,23 @@ import ACMBlueFade from "../../assets/ACMBlueFade.svg"
 
 const cardContents = [
   { title: "Email", value: "contact@acmucsd.org", icon: EmailIcon },
-  { title: "Discord", value: "discord.com/somelink/", icon: DiscordIcon },
+  {
+    title: "Discord",
+    value: "discord.com/somelink",
+    icon: DiscordIcon,
+  },
   { title: "Medium", value: "medium.com/acmucsd", icon: MediumIcon },
-  { title: "Facebook", value: "facebook.com/acmucsd/", icon: FacebookIcon },
+  {
+    title: "Facebook",
+    value: "facebook.com/acmucsd",
+    icon: FacebookIcon,
+  },
   { title: "Github", value: "github.com/acmucsd", icon: GithubIcon },
-  { title: "Instagram", value: "instagram.com/acm.ucsd", icon: InstagramIcon },
+  {
+    title: "Instagram",
+    value: "instagram.com/acm.ucsd",
+    icon: InstagramIcon,
+  },
 ]
 const Footer: React.FC = () => {
   return (
@@ -27,15 +39,18 @@ const Footer: React.FC = () => {
         <div className="footerContents">
           <h2>Connect With Us!</h2>
           <div className="footerContents__contact">
-            {cardContents.map(item => (
-              <div className="footerContents__contact__card">
-                <img src={item.icon} alt={item.title} />
-                <div>
-                  <h3>{item.title}</h3>
-                  <p>{item.value}</p>
+            {cardContents.map(item => {
+              let linkprefix = item.title === "Email" ? "mailto:" : "https://"
+              return (
+                <div className="footerContents__contact__card">
+                  <img src={item.icon} alt={item.title} />
+                  <div>
+                    <h3>{item.title}</h3>
+                    <a href={linkprefix + item.value}>{item.value}</a>
+                  </div>
                 </div>
-              </div>
-            ))}
+              )
+            })}
           </div>
           <div className="footerContents__newsletter">
             <h2>Newsletter</h2>
@@ -46,31 +61,40 @@ const Footer: React.FC = () => {
               name="fname"
               value="my@email.com"
             />
-            <button>Subscribe</button>
+            <button>Subscribe!</button>
           </div>
         </div>
       </div>
       <div className="mobileFooterContainer">
-        <div className="footerContainer--left">
-          <h2>Connect With Us!</h2>
-          <p>acmucsd@gmail.com</p>
-          <div className="community-links">
-            <button>
-              <img src={FacebookIcon} alt="Facebook" />
-            </button>
-            <button>
-              <img src={MediumIcon} alt="Medium" />
-            </button>
-            <button>
-              <img src={GithubIcon} alt="GitHub" />
-            </button>
-            <button>
-              <img src={InstagramIcon} alt="Instagram" />
-            </button>
-            <button>
-              <img src={DiscordIcon} alt="Discord" />
-            </button>
-          </div>
+        <h2>Connect With Us!</h2>
+        <a href="mailto:contact@acmucsd.org">contact@acmucsd.org</a>
+        <div className="community-links">
+          <a href="http://facebook.com/acmucsd/">
+            <img src={FacebookIcon} alt="Facebook" />
+          </a>
+          <a href="http://medium.com/acmucsd">
+            <img src={MediumIcon} alt="Medium" />
+          </a>
+          <a href="http://github.com/acmucsd">
+            <img src={GithubIcon} alt="GitHub" />
+          </a>
+          <a href="http://instagram.com/acm.ucsd">
+            <img src={InstagramIcon} alt="Instagram" />
+          </a>
+          <a href="http://discord.com/somelink/">
+            <img src={DiscordIcon} alt="Discord" />
+          </a>
+        </div>
+        <div className="mobileFooterContainer__newsletter">
+          <h2>Newsletter</h2>
+          <p>Receive weekly events and news!</p>
+          <input
+            type="text"
+            id="emailinput"
+            name="fname"
+            value="my@email.com"
+          />
+          <button>Subscribe!</button>
         </div>
       </div>
     </div>

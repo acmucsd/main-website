eb9d50d0f04c27625ee7a33ba002d54f40f9c22e
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-03-23 11:33:43 -0700

    communities hero text

--- src/sections/community/Community.Hero.tsx
+++ src/sections/community/Community.Hero.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './styles.less'
+import React from "react"
+import "./styles.less"
 
-const CommunityHero: React.FC  = () => {
-    return (
-        <section className="community__hero">
-            Here's the community page
-        </section>
-    )
+const CommunityHero: React.FC = () => {
+  return (
+    <section className="community__hero">
+      <h1>Communities</h1>
+      <p>
+        ACM @ UCSD is one large community, but it is made up of several smaller
+        communities focused on specific areas of technology.
+      </p>
+      <div className="community__hero__communities">
+        insert communities grid
+      </div>
+    </section>
+  )
 }
 
 export default CommunityHero

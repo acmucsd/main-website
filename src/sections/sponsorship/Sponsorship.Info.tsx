97cbc5d0fbbd2f39b62339e3248ff21818865f18
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-04-02 13:48:04 -0700

    added mobile styling

--- src/sections/sponsorship/Sponsorship.Info.tsx
+++ src/sections/sponsorship/Sponsorship.Info.tsx
@@ -2,13 +2,34 @@ import React from "react"
 import "./styles.less"
 
 import SponsorHeroLogo from "../../assets/sponsor_hero.svg"
+import SponsorHeroLogoMobile from "../../assets/sponsor_hero_mobile.svg"
 
 const SponsorshipInfo: React.FC = () => {
   return (
-    <section className="sponsorship__info">
-      <img src={SponsorHeroLogo} alt="Sponsor Logo" />
-      <div className="sponsorship__info__text">
+    <section>
+      <div className="sponsorship__info desktop">
+        <img
+          className="sponsorship__info__img"
+          src={SponsorHeroLogo}
+          alt="Sponsor Logo"
+        />
+        <div className="sponsorship__info__text">
+          <h1 className="sponsorship__info__text__title">Sponsor</h1>
+          <h2 className="sponsorship__info__text__description">
+            Impact a community of 1000+ aspiring
+            <span className="blue"> developers</span>,
+            <span className="pink"> designers</span>, and
+            <span className="purple"> innovators</span>!
+          </h2>
+        </div>
+      </div>
+      <div className="sponsorship__info mobile">
         <h1 className="sponsorship__info__text__title">Sponsor</h1>
+        <img
+          className="sponsorship__info__img"
+          src={SponsorHeroLogo}
+          alt="Sponsor Logo"
+        />
         <h2 className="sponsorship__info__text__description">
           Impact a community of 1000+ aspiring
           <span className="blue"> developers</span>,

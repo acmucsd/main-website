5cc1148b5d204ce9cef45f162a34a154754cfa08
Meshach Adoe <madoe@ucsd.edu>
2021-04-09 01:10:27 +0700

    Created mobile sponsorship page

--- src/sections/sponsorship/Sponsorship.Hero.tsx
+++ src/sections/sponsorship/Sponsorship.Hero.tsx
@@ -9,6 +9,7 @@ const SponsorshipInfo: React.FC  = () => {
             <img src={SponsorImage} alt="Sponsorship" />
             <div className="sponsorship__hero__header">
                 <h1>Sponsor</h1>
+                <img src={SponsorImage} alt="Sponsorship" />
                 <h2>
                     Impact a community of 1000+ aspiring <span className="blue">developers</span>, <span className="pink">designers</span>, and <span className="purple">innovators</span>!
                 </h2>

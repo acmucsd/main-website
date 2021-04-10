b734c0b30a1f671ce8bfde0f919e3333c3e69a8c
Meshach Adoe <63138195+meshachadoe@users.noreply.github.com>
2021-04-10 21:44:26 +0700

    Communities page mobile (#122)
    * initial setup and add mobile navigation
    
    * fix mobile view and styling
    
    * Increased padding for mobile community buttons

--- src/pages/communities.tsx
+++ src/pages/communities.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
@@ -7,12 +7,25 @@ import SEO from "../components/SEO"
 import CommunityHero from "../sections/community/Community.Hero"
 import CommunitySuborgs from "../sections/community/Community.Suborgs"
 
-const CommunitiesPage: React.FC = () => (
-  <Layout>
-    <SEO title="Community" />
-    <CommunityHero />
-    <CommunitySuborgs />
-  </Layout>
-)
+const CommunitiesPage: React.FC = () => {
+  const [windowWidth, setWidth] = useState(window.innerWidth)
+  const isMobile = windowWidth < 812
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  })
+
+  const handleResize = () => {
+    setWidth(window.innerWidth)
+  }
+  return (
+    <Layout>
+      <SEO title="Community" />
+      <CommunityHero isMobile={isMobile}/>
+      <CommunitySuborgs isMobile={isMobile}/>
+    </Layout>
+  )
+}
 
 export default CommunitiesPage

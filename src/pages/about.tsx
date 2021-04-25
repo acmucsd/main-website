966f1ce90a190ba81d00ded3ee257d248539db39
Meshach Adoe <madoe@ucsd.edu>
2021-04-25 19:33:46 +0700

    add mobile styling

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
@@ -9,14 +9,27 @@ import AboutGetInvolved from "../sections/about/About.GetInvolved"
 import AboutMembership from "../sections/about/About.Membership"
 import AboutBoard from "../sections/about/About.Board"
 
-const AboutPage: React.FC = () => (
-  <Layout>
-    <SEO title="About" />
-    <AboutHero />
-    <AboutGetInvolved />
-    <AboutMembership />
-    <AboutBoard />
-  </Layout>
-)
+const AboutPage: React.FC = () => {
+  const [windowWidth, setWidth] = useState(window.innerWidth)
+  const isMobile = windowWidth < 814
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  })
+
+  const handleResize = () => {
+    setWidth(window.innerWidth)
+  }
+  return (
+    <Layout>
+      <SEO title="About" />
+      <AboutHero isMobile={isMobile}/>
+      <AboutGetInvolved isMobile={isMobile}/>
+      {!isMobile && <AboutMembership />}
+      <AboutBoard />
+    </Layout>
+  )
+}
 
 export default AboutPage
--- src/sections/about/About.Hero.tsx
+++ src/sections/about/About.Hero.tsx
@@ -3,14 +3,17 @@ import './styles.less'
 import HeroImage from '../../assets/about-images/about-hero.png'
 
 
-const AboutHero: React.FC  = () => {
+const AboutHero: React.FC<{ isMobile: boolean }>  = ({ isMobile }) => {
     return (
         <section className="about__hero">
-            <section className="about__hero__description">
-                <h1>What is ACM @ UCSD?</h1>
+            <div className="about__hero__description">
+                <div>
+                    <h1>What is ACM @ UCSD?</h1>
+                    {isMobile && <img src={HeroImage} alt="About Hero Image"/>}
+                </div>
                 <h3>We are the Association for Computing Machinery at UCSD! <br/><br/>We are an inclusive member-first community for all who are interested in the field of computing. We welcome all skill levels and majors to our events and activities!</h3>
-            </section>
-            <img src={HeroImage} alt="About Hero Image"/>
+            </div>
+            {!isMobile && <img src={HeroImage} alt="About Hero Image"/>}
         </section>
     )
 }

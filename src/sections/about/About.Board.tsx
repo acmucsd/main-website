db9efe4149f4a1413eb15c93e707f8009bba6c22
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-05-01 17:31:35 -0700

    created board member card

--- src/sections/about/About.Board.tsx
+++ src/sections/about/About.Board.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './styles.less'
-
-const AboutBoard: React.FC  = () => {
-    return (
-        <section className="about__board">
-            
-        </section>
-    )
+import React from "react"
+import "./styles.less"
+import BoardCard from "../../components/BoardCard/BoardCard.tsx"
+const AboutBoard: React.FC = () => {
+  return (
+    <section className="about__board">
+      <BoardCard />
+    </section>
+  )
 }
 
 export default AboutBoard

1b1a3b41ba286e4e4d3aa46819f09c7384eb1497
Steven Steiner <steets250@gmail.com>
2021-11-29 17:49:18 -0800

    Update favicon (#171)

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/ACMBlackLogo.png`, // This path is relative to the root of the site.
+        icon: `src/assets/ACMWhiteLogo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,

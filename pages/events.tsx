7009f4152ad16efa999a528124e4c1870d014359
Faris Ashai <farisashai@gmail.com>
2022-02-25 01:07:35 -0800

    deleted everything

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,27 +1,6 @@
-import { useEffect, useState } from "react";
-import { Size, useWindowSize } from "src/utils";
-
-import SEO from "src/components/SEO";
-
-import HeroImage from "public/assets/about-images/about-hero.png";
-
 const EventsPage: React.FC = () => {
-  // TODO: Mobile responsiveness
-  //   const size: Size = useWindowSize();
-  //   const [isMobile, setIsMobile] = useState(false);
-
-  //   useEffect(() => {
-  //     setIsMobile((size.width || 0) <= 814);
-  //   }, [size]);
-
   return (
     <>
-      <SEO
-        title="About"
-        path="/about"
-        description="We are the Association for Computing Machinery at UCSD! We are an inclusive member-first community for all who are interested in the field of computing. We welcome all skill levels and majors!"
-        image={HeroImage.src}
-      />
       <h1>Events Page</h1>
     </>
   );

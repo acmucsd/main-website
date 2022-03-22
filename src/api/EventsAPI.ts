53d2f8180db1f970eee807640868e1890433d134
Faris Ashai <farisashai@gmail.com>
2022-03-22 16:24:10 -0700

    event page details + calendar actions

--- src/api/EventsAPI.ts
+++ src/api/EventsAPI.ts
@@ -1,3 +1,5 @@
+import * as ics from "ics";
+import { formatURLEventTitle } from "src/utils";
 const EVENT_API = "https://api.acmucsd.com/api/v2/event";
 
 export type EventObject = {
@@ -14,6 +16,7 @@ export type EventObject = {
   pointValue: number;
   requiresStaff: boolean;
   staffPointBonus: number;
+  facebookUrl?: string;
 };
 
 export type EventsArray = EventObject[];
@@ -59,4 +62,53 @@ const getEvent = async (uuid: string): Promise<EventObject | undefined> => {
   }
 };
 
-export { getEvent, getAllEvents };
+const createDownloadFile = (textContent: string, title: string): void => {
+  const element = document.createElement("a");
+  const file = new Blob([textContent], { type: "text/plain" });
+  element.href = URL.createObjectURL(file);
+  element.download = `${title}.ics`;
+  document.body.appendChild(element);
+  element.click();
+};
+
+const downloadICS = (event: EventObject): void => {
+  const startDate = new Date(event.start);
+  const endDate = new Date(event.end);
+  const duration = endDate.getTime() - startDate.getTime();
+
+  const attributes: ics.EventAttributes = {
+    start: [
+      startDate.getFullYear(),
+      startDate.getMonth() + 1,
+      startDate.getDate(),
+      startDate.getHours(),
+      startDate.getMinutes(),
+    ],
+    duration: { minutes: duration / (1000 * 60) },
+    title: event.title,
+    description: event.description,
+    location: event.location,
+    url: `https://acmucsd.com/events/${formatURLEventTitle(event.title)}-${event.uuid}`,
+    organizer: { name: "ACM at UCSD", email: "acm@ucsd.edu" },
+  };
+
+  const response = ics.createEvent(attributes);
+
+  createDownloadFile(response.value, formatURLEventTitle(event.title));
+};
+
+const saveToGoogleCal = ({ title, description, location, start, end }: EventObject): void => {
+  const url = new URL("https://www.google.com/calendar/render?action=TEMPLATE");
+  const params = new URLSearchParams(url.search);
+  params.append("text", title);
+  params.append("details", description);
+  params.append("location", location);
+  const startISO = start.replaceAll("-", "").replaceAll(":", "").replaceAll(".", "");
+  const endISO = end.replaceAll("-", "").replaceAll(":", "").replaceAll(".", "");
+  params.append("dates", `${startISO}/${endISO}`);
+
+  const GCAL_LINK = `${url.origin}${url.pathname}?${params.toString()}`;
+  window.open(GCAL_LINK);
+};
+
+export { getEvent, getAllEvents, downloadICS, saveToGoogleCal };
---
+++ src/sections/event/Event.Content.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+import { downloadICS, EventObject, saveToGoogleCal } from "src/api/EventsAPI";
+import { days, getDateTime, months } from "src/utils";
+import s from "./Event.module.scss";
+
+const EventContent: React.FC<{ event: EventObject }> = ({ event }) => {
+  const month = months[new Date(event.start).getMonth()];
+  const date = new Date(event.start).getDate();
+  const day = days[(new Date(event.start).getDay() + 6) % 7];
+  const time = getDateTime(event).time;
+  const { committee, title, location, description, start, end, facebookUrl } = event;
+
+  return (
+    <div className={s.container}>
+      <div className={s.eventHeader}>
+        <Link href={"/events"}>
+          <a className={s.backButton}>&lt; Back to Events</a>
+        </Link>
+        <h3 className={`${s.eventType} ${s[committee.toLowerCase()]}`}>General Event</h3>
+      </div>
+      <div className={s.eventBody}>
+        <img src={event.cover} className={s.eventCover} alt="Event Cover Photo" />
+        <div className={s.eventInfo}>
+          <div className={s.titleLocationGrid}>
+            <div className={s.date}>
+              <h2 className={s.eventDate}>
+                <b>{month}</b> {date}
+              </h2>
+              <h2 className={s.eventDay}>{day}</h2>
+            </div>
+            <h2 className={s.location}>{location}</h2>
+            <h2 className={`${s.title} ${s[committee.toLowerCase()]}`}>{title}</h2>
+            <h2 className={s.time}>{time}</h2>
+          </div>
+          <div className={s.eventLinks}>
+            <a
+              className={s.eventLink}
+              onClick={e => {
+                e.preventDefault();
+                downloadICS(event);
+              }}
+            >
+              <img src="/assets/calendar.svg" alt="Add to Calendar" width={30} height={30} />
+              Download Calendar Event
+            </a>
+            <a
+              className={s.eventLink}
+              onClick={e => {
+                e.preventDefault();
+                saveToGoogleCal(event);
+              }}
+            >
+              <img src="/assets/calendar.svg" alt="Add to Calendar" width={30} height={30} />
+              Save to Google Calendar
+            </a>
+            {!facebookUrl ? null : (
+              <a className={s.eventLink}>
+                <img
+                  src="/assets/facebook-dark.svg"
+                  alt="View on Facebook"
+                  width={30}
+                  height={30}
+                />
+                View on Facebook
+              </a>
+            )}
+          </div>
+          <p className={s.eventDescription}>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default EventContent;

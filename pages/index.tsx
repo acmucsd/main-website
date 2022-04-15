88cc9e36a3ed1369ebae02775a650c886ffaf058
Faris Ashai <farisashai@gmail.com>
2022-04-15 12:34:36 -0700

    change wording on home page (#246)
    * change wording
    
    * consistent class names with content
    
    * fix wording everywhere

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import HomeHero from "src/sections/home/Home.Hero";
 import HomeCommunities from "src/sections/home/Home.Communities";
-import HomeJoinTheClub from "src/sections/home/Home.JoinTheClub";
+import HomeJoinUs from "src/sections/home/Home.JoinUs";
 import HomeEvents from "src/sections/home/Home.Events";
 import SEO from "src/components/SEO";
 import CoolLogo from "public/assets/cool_logo_1000x1000.png";
@@ -11,11 +11,11 @@ const IndexPage: React.FC<{ events: Array<EventObject> }> = ({ events }) => (
     <SEO
       title="ACM at UCSD"
       path="/"
-      description="Join the club. ACM at UCSD is an inclusive community of students passionate about technology. 1000+ UCSD members. 120+ annual events. 850+ cups of boba served."
+      description="Join us. ACM at UCSD is an inclusive community of students passionate about technology. 1000+ UCSD members. 120+ annual events. 850+ cups of boba served."
       image={CoolLogo.src}
     />
     <HomeHero />
-    <HomeJoinTheClub />
+    <HomeJoinUs />
     <HomeEvents events={events} />
     <HomeCommunities />
   </>
---
+++ src/sections/home/Home.JoinUs.tsx
@@ -0,0 +1,23 @@
+import Number from "src/components/Statistic";
+
+const HomeJoinUs: React.FC = () => {
+  return (
+    <section className="home__join-us">
+      <div className="home__join-us__grid">
+        <div className="home__join-us__grid__header">
+          <h1>Join us.</h1>
+          <p>
+            ACM Chapter at UCSD is an inclusive community of students passionate about technology.
+          </p>
+        </div>
+        <div className="home__join-us__grid__numbers">
+          <Number color="#FF6F6F" description="ucsd members" extension="+" number={1000} />
+          <Number color="#F9A857" description="annual events" extension="+" number={120} />
+          <Number color="#51C0C0" description="cups of boba" extension="+" number={850} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeJoinUs;

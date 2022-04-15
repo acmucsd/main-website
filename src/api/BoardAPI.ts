c35284864dc5758b7d9f4fcc39da9707bafe9ebd
Faris Ashai <farisashai@gmail.com>
2022-04-14 22:27:21 -0700

    small tweaks to documentation

--- src/api/BoardAPI.ts
+++ src/api/BoardAPI.ts
@@ -2,31 +2,26 @@ import { BoardMemberProps } from "src/types";
 
 import {
   GoogleSpreadsheet,
-  GoogleSpreadsheetRow,
   GoogleSpreadsheetWorksheet,
-  PaginationOptions,
   ServiceAccountCredentials,
 } from "google-spreadsheet";
 
+// Take raw data from row, if its blank do nothing
+// Otherwise, consistently format as a LinkedIn profile url (some people may provide only the username portion on the spreadsheet)
 const formatLinkedIn = (val: string): string => {
-  return !val
-    ? val
-    : val.includes("https://www.linkedin.com/in/")
-    ? val
-    : `https://www.linkedin.com/in/${val}`;
+  if (!val) return null;
+  return val.includes("https://www.linkedin.com/in/") ? val : `https://www.linkedin.com/in/${val}`;
 };
 
 export const getBoardData = async () => {
-  const BOARD_SPREADSHEET_ID = "1YAaNjThIEkgiM9k6-itHw6f33aFbjZt3XspD80TLHi0";
-  const SERVICE_ACC_EMAIL = process.env.GOOGLE_SERVICE_ACC_EMAIL;
-  const PRIVATE_KEY = process.env.GOOGLE_SERVICE_KEY_PRIVATE;
+  const { BOARD_SPREADSHEET_ID, GOOGLE_SERVICE_ACC_EMAIL, GOOGLE_SERVICE_KEY_PRIVATE } =
+    process.env;
 
-  console.log(process.env.SPREADSHEET_URL);
   const doc = new GoogleSpreadsheet(BOARD_SPREADSHEET_ID);
 
   const credentials: ServiceAccountCredentials = {
-    client_email: SERVICE_ACC_EMAIL,
-    private_key: PRIVATE_KEY,
+    client_email: GOOGLE_SERVICE_ACC_EMAIL,
+    private_key: GOOGLE_SERVICE_KEY_PRIVATE,
   };
 
   await doc.useServiceAccountAuth(credentials);
@@ -36,9 +31,9 @@ export const getBoardData = async () => {
 
   // Load the spreadsheet at index 0, when the board data for a new year is added as a worksheet, it will automatically select from there instead
   const boardSheet: GoogleSpreadsheetWorksheet = doc.sheetsByIndex[0];
+
   // Headers should be in the second row after the first row with title "ACM Board 20XX-20XX"
   await boardSheet.loadHeaderRow(2);
-
   const rows = await boardSheet.getRows();
 
   // Map through array of rows, each one is an object with column headers as keys
@@ -59,8 +54,6 @@ export const getBoardData = async () => {
     .filter(user => user.org && user.org !== "members at large") // valid suborg is required
     .filter(user => user.title) // position title is required
     .filter(user => user.profile_image); // image url is required
-
-  console.log(boardMembers);
 
   return boardMembers;
 };

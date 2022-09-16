e1db91d38117c2db95ffe4fa36c6539bb64d0c25
Faris Ashai <farisashai@gmail.com>
2022-09-16 14:54:20 -0700

    Add new sponsor logos

--- src/sections/sponsorship/Sponsorship.Logos.tsx
+++ src/sections/sponsorship/Sponsorship.Logos.tsx
@@ -5,13 +5,13 @@ import TESCImg from "public/assets/sponsor-images/tesc_logo.png";
 import RobinhoodImg from "public/assets/sponsor-images/robinhood_logo.png";
 import GearImg from "public/assets/sponsor-images/gear_logo.png";
 import LockheedMartin from "public/assets/sponsor-images/lockheed_martin_logo.png";
+import Roblox from "public/assets/sponsor-images/roblox.svg";
+import JaneStreet from "public/assets/sponsor-images/jane_street.png";
 
 const SponsorshipLogos: React.FC = () => {
   return (
     <section className="sponsorship__logos">
-      <h1 className="sponsorship__logos__header">
-        Thank you to our current sponsors!
-      </h1>
+      <h1 className="sponsorship__logos__header">Thank you to our current sponsors!</h1>
       <div className="sponsorship__logos__img-grid">
         <a href="https://gear-tech.io/">
           <img src={GearImg.src} alt="Gear" />
@@ -34,6 +34,14 @@ const SponsorshipLogos: React.FC = () => {
         </a>
         <a href="https://tesc.ucsd.edu/">
           <img src={TESCImg.src} alt="TESC" />
+        </a>
+      </div>
+      <div className="sponsorship__logos__img-grid">
+        <a href="https://www.roblox.com/">
+          <img src={Roblox.src} alt="Roblox" />
+        </a>
+        <a href="https://www.janestreet.com/">
+          <img src={JaneStreet.src} alt="Jane Street" />
         </a>
       </div>
     </section>

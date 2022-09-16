1f8ca820fdb883c0f47364a4326e3411058af27d
Faris Ashai <farisashai@gmail.com>
2022-09-16 15:42:59 -0700

    Fetch sponsor logos from spreadsheet

--- pages/sponsor.tsx
+++ pages/sponsor.tsx
@@ -6,7 +6,10 @@ import SponsorshipBenefits from "src/sections/sponsorship/Sponsorship.Benefits";
 import SponsorHeroLogo from "public/assets/sponsor-images/sponsor_hero.png";
 import { NextPage } from "next";
 
-const SponsorshipPage: NextPage = () => (
+import { getSponsorsData } from "src/api/SponsorAPI";
+import { SponsorProps } from "src/types";
+
+const SponsorshipPage: NextPage<{ sponsors: SponsorProps[] }> = ({ sponsors }) => (
   <>
     <SEO
       title="Sponsor | ACM at UCSD"
@@ -15,9 +18,19 @@ const SponsorshipPage: NextPage = () => (
       image={SponsorHeroLogo.src}
     />
     <SponsorshipInfo image={SponsorHeroLogo.src} />
-    <SponsorshipLogo />
+    <SponsorshipLogo sponsors={sponsors} />
     <SponsorshipBenefits />
   </>
 );
 
 export default SponsorshipPage;
+
+export async function getStaticProps() {
+  const sponsorsData = await getSponsorsData();
+  return {
+    props: {
+      sponsors: sponsorsData || [],
+    },
+    revalidate: 1 * 60 * 60 * 12, // generate once every 12 hours (in seconds)
+  };
+}

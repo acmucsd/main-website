4fc11f5876ed04cd8b639bc13d047319b5f53609
adahe8 <43501898+adahe8@users.noreply.github.com>
2023-03-29 20:37:24 -0700

    CalendarModal design change
    Reverting unintentional change revert that I did on accident (in the wrong branch oops) while trying to make a pull request.

--- src/components/CalendarModal/index.tsx
+++ src/components/CalendarModal/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { EventObject, saveToAppleCal, saveToGoogleCal } from "src/api/EventsAPI";
 import s from "./CalendarModal.module.scss";
+import GoogleCalendarLogo from "public/assets/googlecalendar.png";
+import AppleCalendarLogo from "public/assets/applecalendar.png";
 
 interface CalendarModalProps {
   show: boolean;
@@ -11,13 +13,19 @@ interface CalendarModalProps {
 export const CalendarModal: React.FC<CalendarModalProps> = ({ show, event, onClose }) => (
   <dialog className={s.modal} open={show}>
     <section className={s["modal-main"]}>
-      <h3>What type of calendar event would you like?</h3>
+      <h3>Add Event to Calendar</h3>
       <div className={s.options}>
-        <button onClick={() => saveToAppleCal(event)}>Apple Calendar</button>
-        <button onClick={() => saveToGoogleCal(event)}>Google Calendar</button>
+        <button className={s.google} onClick={() => saveToGoogleCal(event)}>
+          <img src={GoogleCalendarLogo.src} alt="google calendar" />
+          <b>Google Calendar</b>
+        </button>
+        <button className={s.apple} onClick={() => saveToAppleCal(event)}>
+          <img src={AppleCalendarLogo.src} alt="apple calendar" />
+          <b>Apple Calendar</b>
+        </button>
       </div>
       <br />
-      <button onClick={onClose}>Cancel</button>
+      <button onClick={onClose}><b>Cancel</b></button>
     </section>
   </dialog>
 );

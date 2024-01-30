5965dfa69881e0e53eccf67bd2adfaa102ffff19
CarmenHe <43501759+CarmenHe@users.noreply.github.com>
2024-01-29 18:52:31 -0800

    Button on events card opens dialog with share options

--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -2,15 +2,18 @@ import Link from "next/link";
 import { saveToAppleCal, EventObject, saveToGoogleCal } from "src/api/EventsAPI";
 import { useState } from "react";
 import { CalendarModal } from "../CalendarModal";
+import { ShareToModal } from "../ShareToModal";
 import s from "./EventCard.module.scss";
 import { days, months, formatURLEventTitle, getDateTime, withHttp } from "src/utils/general";
 import {  FaCalendarAlt } from "react-icons/fa";
+import { FaShareAlt } from "react-icons/fa";
 import { GRAY } from "src/utils/constants";
 
 const EventCard: React.FC<{
   event: EventObject;
 }> = ({ event }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCalModalOpen, setIsCalModalOpen] = useState(false);
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false);
   const month = months[new Date(event.start).getMonth()];
   const date = new Date(event.start).getDate();
   const day = days[new Date(event.start).getDay()];
@@ -18,7 +21,8 @@ const EventCard: React.FC<{
   const { committee, uuid, title, location, eventLink } = event;
   return (
     <>
-      <CalendarModal show={isModalOpen} onClose={() => setIsModalOpen(false)} event={event} />
+      <CalendarModal show={isCalModalOpen} onClose={() => setIsCalModalOpen(false)} event={event} />
+      <ShareToModal show={isShareModalOpen} onClose={() => setIsShareModalOpen(false)} event={event} />
       <Link href={`/events/${formatURLEventTitle(title)}-${uuid}`} passHref>
         <div className={s.card}>
           <div className={s.cardHeader}>
@@ -36,11 +40,19 @@ const EventCard: React.FC<{
           <div className={s.cardFooter}>
             <a
               onClick={e => {
-                setIsModalOpen(true);
+                setIsCalModalOpen(true);
                 e.stopPropagation();
               }}
             >
               <FaCalendarAlt color={GRAY} size={25} />
+            </a>
+            <a
+              onClick={e => {
+                setIsShareModalOpen(true);
+                e.stopPropagation();
+              }}
+            >
+              <FaShareAlt color={GRAY} size={25} />
             </a>
           </div>
         </div>
